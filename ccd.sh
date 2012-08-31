8d6947d85783af1c26c6e11789500714d5cf7c24
Liam McMahon <lmmcmahon9@gmail.com>
2012-08-31 17:08:41 -0500

    Added README (have to write intall script)

--- ccd.sh
+++ ccd.sh
@@ -1,12 +1,10 @@
-#! /bin/bash
-
 CCD_DICTIONARY_PATH="/home/liam/Documents/code/ccd/dictionary.txt"
 
 function ccd() {
     if [[ $1 == "add" ]]; then
 	#$2 is the name to give to the current directory
 	echo "$2 $(pwd)" >> $CCD_DICTIONARY_PATH
-    elif [[ $1 == "--help" ]]; then
+    elif [[ $1 == "--help" || $1 == "help" ]]; then
 	echo "Usage: "
 	echo "ccd add [shortcut]"
 	echo "    creates a ccd shortcut for the current directory"
@@ -14,7 +12,7 @@ function ccd() {
 	echo "    cds to the directory for the specified shortcut"
 	echo "ccd list"
 	echo "    prints the mapping of shortcuts to directories"
-    elif [[ $1 == "list" ]]; then
+    elif [[ $1 == "list" || $1 == "--list" ]]; then
 	cat $CCD_DICTIONARY_PATH
     else
         #$1 is the destination name
