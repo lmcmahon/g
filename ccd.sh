9b019c9faccd71c32d23bca7140026b968885614
Liam McMahon <lmmcmahon9@gmail.com>
2012-08-31 16:51:37 -0500

    It now works and has some help text

--- ccd.sh
+++ ccd.sh
@@ -3,21 +3,25 @@
 CCD_DICTIONARY_PATH="/home/liam/Documents/code/ccd/dictionary.txt"
 
 function ccd() {
-    #$1 is the destination name
-    echo s1
-    echo $1
-    temp1="$(grep ja /home/liam/Documents/code/ccd/dictionary.txt)"
+    if [[ $1 == "add" ]]; then
+	#$2 is the name to give to the current directory
+	echo "$2 $(pwd)" >> $CCD_DICTIONARY_PATH
+    elif [[ $1 == "--help" ]]; then
+	echo "Usage: "
+	echo "ccd add [shortcut]"
+	echo "    creates a ccd shortcut for the current directory"
+	echo "ccd [shortcut]"
+	echo "    cds to the directory for the specified shortcut"
+	echo "ccd list"
+	echo "    prints the mapping of shortcuts to directories"
+    elif [[ $1 == "list" ]]; then
+	cat $CCD_DICTIONARY_PATH
+    else
+        #$1 is the destination name
+	temp0="^$1 "
+	temp1=("$(grep $temp0 /home/liam/Documents/code/ccd/dictionary.txt)")
 
-    echo temp1
-    echo $temp1
-
-    temp2="$(echo $temp1 | sed s/\ /\
-/)"
-
-    echo temp2
-    echo $temp2
-
-    temp3="$(echo $temp2 | tail -n 2)"
-    echo temp3
-    echo $temp3
+	temp2=($temp1)
+	cd ${temp2[1]}
+    fi
 }
